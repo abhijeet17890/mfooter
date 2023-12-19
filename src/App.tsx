2f672a4a02811e19ed73baea462fc10cff4335ab
Abhijeet Gupta <abhijeet17890@gmail.com>
2023-12-19 12:12:20 +0530

    footer code added

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import MFooter from "./components/MFooter";
+import "./index.css";
+
+const App = () => (
+  <div className="">
+    <MFooter />
+  </div>
+);
+ReactDOM.render(<App />, document.getElementById("app"));
---
+++ src/components/MFooter/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./index.css";
+const MFooter = () => {
+  return (
+    <div className="footer-div">
+        This is footer
+        {/* <button>footer button</button> */}
+    </div>
+  )
+  };
+
+  export default MFooter;
